3fb89cbb64810ebb08324a6edf666a0de8db51bb
laineyberry <laineyberry@cmail.carleton.ca>
2024-11-30 02:18:31 -0500

    added everything!

---
+++ lfd game.js
@@ -0,0 +1,64 @@
+
+const acc = [
+    "acc1.png",
+    "acc2.png",
+    "acc3.png",
+    "acc4.png",
+    "acc5.png",
+    "acc6.png",
+    "acc7.png",
+    "acc8.png",
+    "acc9.png"
+];
+
+const hair = [
+
+    "hair1.png",
+    "hair2.png",
+    "hair3.png",
+    "hair4.png",
+    "hair5.png",
+    "hair6.png",
+    "hair7.png",
+    "hair8.png"
+
+];
+
+const head = [
+
+    "head1.png",
+    "head2.png",
+    "head3.png",
+    "head4.png",
+    "head5.png",
+    "head6.png",
+    "head7.png",
+    "head8.png",
+    "head9.png",
+    "head10.png"
+
+];
+
+const outfit = [
+
+    "outfit1.png",
+    "outfit2.png",
+    "outfit3.png",
+    "outfit4.png",
+    "outfit5.png",
+    "outfit6.png"
+
+];
+
+const background = [
+
+    "back1.png",
+    "back2.png",
+    "back3.png",
+    "back4.png",
+    "back5.png",
+    "back6.png",
+    "back7.png"
+
+];
+
