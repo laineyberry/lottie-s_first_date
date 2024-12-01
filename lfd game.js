f4959612a8aca9fbc0011ac5eb34a2e49353e73d
laineyberry <laineyberry@cmail.carleton.ca>
2024-12-01 01:36:12 -0500

    done!

--- lfd game.js
+++ lfd game.js
@@ -8,7 +8,9 @@ const acc = [
     "acc6.png",
     "acc7.png",
     "acc8.png",
-    "acc9.png"
+    "acc9.png",
+    "acc10.png",
+    "blank.png"
 ];
 
 const hair = [
@@ -16,11 +18,13 @@ const hair = [
     "hair1.png",
     "hair2.png",
     "hair3.png",
-    "hair4.png",
     "hair5.png",
     "hair6.png",
-    "hair7.png",
-    "hair8.png"
+    "hair8.png",
+    "hair9.png",
+    "hair10.png",
+    "hair11.png",
+    "blank.png"
 
 ];
 
@@ -35,7 +39,9 @@ const head = [
     "head7.png",
     "head8.png",
     "head9.png",
-    "head10.png"
+    "head10.png",
+    "head11.png",
+    "blank.png"
 
 ];
 
@@ -46,12 +52,14 @@ const outfit = [
     "outfit3.png",
     "outfit4.png",
     "outfit5.png",
-    "outfit6.png"
+    "outfit6.png",
+    "outfit7.png",
+    "outfit9.png",
+    "blank.png"
 
 ];
 
 const background = [
-
     "back1.png",
     "back2.png",
     "back3.png",
@@ -59,6 +67,122 @@ const background = [
     "back5.png",
     "back6.png",
     "back7.png"
-
 ];
 
+let accI = 0;
+let hairI = 0;
+let outfitI = 0;
+let headI = 0;
+let backI = 5;
+
+function changeAcc() {
+
+    const layer = document.getElementById("acclayer");
+
+    accI += 1;
+
+    if (accI >= acc.length) {
+
+        accI = 0;
+    }
+
+    layer.src = acc[accI]
+
+}
+
+function changeHair() {
+
+    const layer = document.getElementById("hairlayer");
+
+    hairI += 1;
+
+    if (hairI >= hair.length) {
+
+        hairI = 0;
+    }
+
+    layer.src = hair[hairI]
+
+}
+function changeOutfit() {
+
+    const layer = document.getElementById("outfitlayer");
+
+    outfitI += 1;
+
+    if (outfitI >= outfit.length) {
+
+        outfitI = 0;
+    }
+
+    layer.src = outfit[outfitI]
+
+}
+
+function changeHead() {
+
+    const layer = document.getElementById("headlayer");
+
+    headI += 1;
+
+    if (headI >= head.length) {
+
+        headI = 0;
+    }
+
+    layer.src = head[headI]
+
+}
+
+function changeBack() {
+    const backpng = document.getElementById("backgroundlayer");
+
+    backI += 1;
+    if (backI >= background.length) {
+        backI = 0;
+    }
+
+    backpng.src = background[backI];
+}
+
+function reset() {
+
+    const hairLayer = document.getElementById("hairlayer");
+    const outfitLayer = document.getElementById("outfitlayer");
+    const headLayer = document.getElementById("headlayer");
+    const accLayer = document.getElementById("acclayer");
+    const backgroundLayer = document.getElementById("backgroundlayer");
+    hairLayer.src = "blank.png";
+    outfitLayer.src = "blank.png";
+    headLayer.src = "blank.png";
+    accLayer.src = "blank.png";
+    backgroundLayer.src = "back6.png";
+
+
+}
+
+function menuReturn() {
+
+    window.location.href = "index.html";
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+document.getElementById("accbtn").addEventListener("click", changeAcc);
+document.getElementById("hairbtn").addEventListener("click", changeHair);
+document.getElementById("headbtn").addEventListener("click", changeHead);
+document.getElementById("outfitbtn").addEventListener("click", changeOutfit);
+document.getElementById("backbtn").addEventListener("click", changeBack);
+
+document.getElementById("resetbtn").addEventListener("click", reset);
+document.getElementById("menubtn").addEventListener("click", menuReturn);
+
+});
+
+window.onload = function () {
+    const backpng = document.getElementById("backgroundlayer");
+    backpng.src = background[backI]; // Ensure backI points to back6.png
+};
+
+console.log("Script is loaded");
